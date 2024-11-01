4c53035efdff3551e4bc6b7f276a3b19f438eaef
samuel12318 <samuelbonney295@gmail.com>
2024-11-01 11:59:22 +0000

    add a book app

---
+++ book.py
@@ -0,0 +1,40 @@
+class BookInventory:
+    
+    def __init__(self):
+        self.books = {}
+        self.next_id = 1
+
+    def add_book(self, title, author, number_of_pages):
+        book_id = self.next_id
+        self.books[book_id] = {
+        'title': title,
+        'author': author,
+        'number_of_pages': number_of_pages
+        }
+        self.next_id += 1
+        return book_id
+
+    def remove_book(self, book_id):
+        if book_id in self.books:
+            del self.books[book_id]
+        else:
+            raise ValueError("Book not found")
+
+    def edit_book(self, book_id, title, author, number_of_pages):
+        if book_id in self.books:
+            self.books[book_id] = {
+            'title': title,
+            'author': author,
+            'number_of_pages': number_of_pages
+        }
+        else:
+            raise ValueError("Book not found")
+
+    def view_all(self):
+        return self.books.values()
+
+    def view_by_id(self, book_id):
+        if book_id in self.books:
+            return self.books[book_id]
+        else:
+            raise ValueError("Book not found")
---
+++ test_book.py
@@ -0,0 +1,44 @@
+import unittest
+from book import BookInventory
+
+class TestBookInventory(unittest.TestCase):
+    def setUp(self):
+        self.inventory = BookInventory()
+
+    def test_add_book(self):
+        book_id = self.inventory.add_book("The Kid", "Abena", 208)
+        book = self.inventory.view_by_id(book_id)
+        self.assertEqual(book['title'], "The Kid")
+        self.assertEqual(book['author'], "Abena")
+        self.assertEqual(book['number_of_pages'], 208)
+
+    def test_remove_book(self):
+        book_id = self.inventory.add_book("The book", "Paul", 67)
+        self.inventory.remove_book(book_id)
+        with self.assertRaises(ValueError):
+            self.inventory.view_by_id(book_id)
+
+    def test_edit_book(self):
+        book_id = self.inventory.add_book("Shoe", "Esi", 60)
+        self.inventory.edit_book(book_id, "The Hat", "Akua", 200)
+        book = self.inventory.view_by_id(book_id)
+        self.assertEqual(book['title'], "The Hat")
+        self.assertEqual(book['author'], "Akua")
+        self.assertEqual(book['number_of_pages'], 200)
+
+    def test_view_all(self):
+        self.inventory.add_book("The red Hat", "Ama", 708)
+        self.inventory.add_book("The blue Hat", "Kojo", 708)
+        books = self.inventory.view_all()
+        self.assertEqual(len(books), 2)
+
+
+    def test_view_by_id(self):
+        book_id = self.inventory.add_book("The red Hat", "Ama", 708)
+        book = self.inventory.view_by_id(book_id)
+        self.assertEqual(book['title'], "The red Hat")
+        self.assertEqual(book['author'], "Ama")
+        self.assertEqual(book['number_of_pages'], 708)
+
+if __name__ == '__main__':
+    unittest.main()
